f674b142ab96386f88d2610b0506d77574d46596
mohammadtk1994 <mohammadtavakolikia66@gmail.com>
2023-06-17 17:42:39 +0330

    theme and topbar has been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+
 function App() {
+  const [theme, colorMode] = useMode();
   return (
     <>
-      <h1>MT Admin Dashboard</h1>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <main className="content">
+              <Topbar />
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </>
   );
 }
